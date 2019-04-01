78709346f2ad0cefd015e1f3835ce50223382186
Heru Santoso <herusantoso008@gmail.com>
2019-04-01 17:22:55 +0700

    Commit : add api logout

---
+++ src/main/java/com/herusantoso/tokonezia/resource/AuthResource.java
@@ -0,0 +1,38 @@
+package com.herusantoso.tokonezia.resource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1")
+public class AuthResource {
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Boolean> logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String tokenValue = authHeader.replace("Bearer", "").trim();
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+            tokenStore.removeAccessToken(accessToken);
+        }
+        return ResponseEntity.ok()
+                .body(Boolean.TRUE);
+    }
+}
